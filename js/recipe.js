7aa7bdb77a59c30eb1aba23c55740605fbd7b722
unknown <anargahramanov@gmail.com>
2018-03-31 16:20:12 +0400

    pop-up deleted, recipe_page added

---
+++ js/recipe.js
@@ -0,0 +1,4 @@
+var videoDiv = document.getElementById("video");
+var videoFrame = document.getElementById("videoFrame");
+
+videoDiv.style.height = (videoDiv.scrollWidth * 0.56) + "px";
--- js/script.js
+++ js/script.js
@@ -5,12 +5,6 @@ var li = $("#navMenu ul li");
 var lis = document.querySelectorAll("#navMenu ul li");
 var liImg = $("#navMenu ul li img");
 
-// VideoPage JS
-var mainVideoPage = document.getElementById("mainVideoPage");
-var iframe = document.querySelector("#mainVideoPage #videoSide iframe");
-var closeBtn = document.getElementById("closeBtn")
-var button = document.getElementById("button");
-
 // SliderSec JS
 var slider = document.querySelector("#slider");
 var slideImgs = document.querySelectorAll("#slider #sliderImgs img");
@@ -23,9 +17,6 @@ var prev = document.querySelector("#slider .prev");
 // Foods Sec
 var cols = document.querySelectorAll("#BreakfastSec .rows .cols");
 var colsImg = document.querySelectorAll("#BreakfastSec .rows .cols .colsPhoto img");
-
-
-mainVideoPage.style.height = body.scrollHeight + "px";
 
 // NavBarMenu Icons
 $("#navMenu ul li").each(function () {
@@ -98,40 +89,8 @@ setInterval(function () {
     nextSlide();
 }, 3000);
 
-// function slideArrows() {
-//     next.addEventListener("click", function () {
-//         nextSlide();
-//     });
-
-//     prev.addEventListener("click", function () {
-//         prevSlide();
-
-//     });
-
-//     slider.onmouseover = function () {
-//         arrows.forEach(function (a) {
-//             a.style.opacity = "1";
-//             a.style.transition = "0.5s";
-//         });
-
-//     };
-
-//     slider.onmouseleave = function () {
-//         arrows.forEach(function (a) {
-//             a.style.opacity = "0";
-//             a.style.transition = "0.5s";
-//         });
-
-//     };
-// };
-
 // Main Page Reciepes
 cols.forEach(function (col) {
-    col.onclick = function () {
-        mainVideoPage.style.display = "block";
-        var src = col.children[0].children[0].getAttribute("title");
-        iframe.src = src;
-    };
     col.onmouseover = function () {
         col.style.transform = "scale(1.05)";
         col.style.boxShadow = "#a4a4a480 5px 10px 5px";
@@ -143,11 +102,6 @@ cols.forEach(function (col) {
         col.style.transform = null;
         col.style.boxShadow = null;
     };
-});
-
-closeBtn.addEventListener("click", function () {
-    mainVideoPage.style.display = null;
-    iframe.src = null;
 });
 
 window.onkeyup = function (keyClose) {
